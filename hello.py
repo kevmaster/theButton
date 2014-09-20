5793ba477163a62596908153ec06395d9d627783
kevmaster <scheer2@illinois.edu>
2014-09-20 15:37:00 -0500

    added highscores

--- hello.py
+++ hello.py
@@ -1,9 +1,24 @@
 from flask import Flask
 import time
-app = Flask(__name__)
+#app = Flask(__name__)
 
 #def main():
 #app.g = 0
+def create_app():
+	app = Flask(__name__)
+	app.g = [0,0,0]
+	def gethighscore():
+		highscore = open('highscore','r')
+		highscore = highscore.read()
+		app.g[2] = highscore
+	gethighscore()
+	return app
+
+@atexit
+def savehighscore():
+	highscore = open('highscore','w')
+	highscore.write(app.g[2])
+	return
 
 @app.route("/timer", methods=['GET'])
 def timer():
@@ -22,6 +37,10 @@ def push():
 def release():
 	app.g[0] = app.g[0] - 1
 	if app.g[0]==0:
+		number = time.time()-app.g[1]
+		if number>app.g[2]:
+			app.g[2]=number
+			savehighscore()
 		app.g[1] = time.time()
 	return str(app.g[0])
 
@@ -29,7 +48,12 @@ def release():
 def stack():
 	return str(app.g[0])
 
+@app.route("/highscore", methods = ['GET'])
+def highscore():
+	return str(int(app.g[2]))
+
 if __name__ == "__main__":
-	app.g = [0,0]
+#	app.g = [0,0,0]
+	app = create_app()
 	app.debug = True
 	app.run(host = '0.0.0.0', debug= True)
