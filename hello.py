31a02ee9a746f0f4fc2b2315838e334c63df3aa3
kevmaster <scheer2@illinois.edu>
2014-09-20 18:36:09 -0500

    now tracks ips and number of pushes and releases

--- hello.py
+++ hello.py
@@ -1,42 +1,85 @@
 from flask import Flask
+from flask import request
 import time
-#app = Flask(__name__)
-
-#def main():
-#app.g = 0
+import json
+#runs at startup, sets up the server and variables
 def create_app():
 	app = Flask(__name__)
-	app.g = [0,0,0]
+	app.g = [0,0,0,[]]
 	def gethighscore():
 		highscore = open('highscore','r')
 		highscore = highscore.read()
 		app.g[2] = highscore
+	def setupdb():
+		db = open('ips','r')
+		db = json.load(db)
+		app.g[3] = db
 	gethighscore()
+	setupdb()
 	return app
+
+#app.g[0] is number of buttons currently pushed
+#app.g[1] is timer variable (last time button wasn't pushed)
+#app.g[2] is current high score
+#app.g[3] is database of IPs and number of button presses
 
 app = create_app()
 
+#saves high score to a file
 def savehighscore():
 	highscore = open('highscore','w')
 	highscore.write(str(app.g[2]))
+	highscore.close()
 	return
 
+
+def savedb():
+	db = open('ips','w')
+	json.dump(app.g[3],db)
+	db.close()
+
+def addpush(ip):
+	for x in app.g[3]:
+		if x[0] == ip:
+			x[1] = x[1]+1
+			savedb()
+			return
+	app.g[3].append([ip,1,0])
+	savedb()
+	return
+
+def addrelease(ip):
+	for x in app.g[3]:
+		if x[0] == ip:
+			x[2] = x[2]+1
+			savedb()
+			return
+	app.g[3].append([ip,1,0])
+	savedb()
+	return
+
+#returns the current timer time
 @app.route("/timer", methods=['GET'])
 def timer():
 	if app.g[0] == 0:
 		app.g[1] = time.time()
 	return str(int(time.time() - app.g[1]))
 
+#adds one to the buttons pushed variable
 @app.route("/push", methods = ['POST'])
 def push():
-	print 'pusing button'
+	addpush(request.remote_addr)
+	print 'pushing button'
 	if app.g[0]==0:
 		app.g[1] = time.time()
 	app.g[0] = app.g[0] + 1
 	return timer()
 
+#removes one from the buttons pushed variable, resets timer if its the last button,
+#and sets high score if higher than last one
 @app.route("/release", methods = ['POST'])
 def release():
+	addrelease(request.remote_addr)
 	number = int(time.time()-app.g[1])
 	print 'releasing button with current score: '+str(number)+' and old highscore: '+str(app.g[2])
 	app.g[0] = app.g[0] - 1
@@ -48,16 +91,16 @@ def release():
 		app.g[1] = time.time()
 	return str(app.g[0])
 
+#returns number of pushed buttons
 @app.route("/stack", methods = ['POST'])
 def stack():
 	return str(app.g[0])
 
+#returns current highscore
 @app.route("/highscore", methods = ['GET'])
 def highscore():
 	return str(app.g[2])
 
 if __name__ == "__main__":
-#	app.g = [0,0,0]
-#	app = create_app()
 	app.debug = True
 	app.run(host = '0.0.0.0', debug= True)
