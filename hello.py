2577e77e52b02ed36549848ce5f8014ac034f1a6
kevmaster <scheer2@illinois.edu>
2014-09-20 11:57:48 -0500

    changed stack to global var

--- hello.py
+++ hello.py
@@ -3,30 +3,29 @@ import time
 app = Flask(__name__)
 
 #def main():
-stack = 0
 #app.g = 0
 
 @app.route("/timer", methods=['GET'])
 def timer():
-	if stack == 0:
-		app.g = time.time()
-	return str(int(time.time() - app.g))
+	if app.g[0] == 0:
+		app.g[1] = time.time()
+	return str(int(time.time() - app.g[1]))
 
 @app.route("/push", methods = ['POST'])
 def push():
-	if stack==0:
-		app.g = time.time()
-	stack = stack + 1
+	if app.g[0]==0:
+		app.g[1] = time.time()
+	app.g[0] = app.g[0] + 1
 	return timer()
 
 @app.route("/release", methods = ['POST'])
 def release():
-	stack = stack - 1
-	if stack==0:
-		app.g = time.time()
-	return str(stack)
+	app.g[0] = app.g[0] - 1
+	if app.g[0]==0:
+		app.g[1] = time.time()
+	return str(app.g[0])
 
 if __name__ == "__main__":
-	app.g = 0
+	app.g = [0,0]
 	app.debug = True
 	app.run(host = '0.0.0.0', debug= True)
